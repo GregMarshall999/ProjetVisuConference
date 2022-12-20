e437a0ba1786def6b5c10d526ad747a49c0538f0
Gregory Marshall <gregory.marshall999@gmail.com>
2022-12-20 15:41:55 +0100

    Validation

--- VisioConference.Main/Models/Movie.cs
+++ VisioConference.Main/Models/Movie.cs
@@ -6,15 +6,28 @@ namespace VisioConference.Main.Models
 	public class Movie
 	{
 		public int Id { get; set; }
+
+		[Required]
+		[StringLength(60, MinimumLength = 3)]
 		public string? Title { get; set; }
 
 		[Display(Name = "Release Date")]
 		[DataType(DataType.Date)]
 		public DateTime ReleaseDate { get; set; }
+
+		[Required]
+		[StringLength (30)]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
 		public string? Genre { get; set; }
 
+		[Range(1, 100)]
+		[DataType(DataType.Currency)]
 		[Column(TypeName = "decimal(18, 2)")]
 		public decimal Price { get; set; }
-		public string? Rating { get; set; }
+
+		[Required]
+		[StringLength(5)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string? Rating { get; set; }
 	}
 }
