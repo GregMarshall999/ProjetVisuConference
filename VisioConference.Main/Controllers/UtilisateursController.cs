8575dbb9bbb5c337b4397bd9f43a105d6366144d
Gregory Marshall <gregory.marshall999@gmail.com>
2022-12-21 09:48:42 +0100

    Controlleur Utilisateur

--- VisioConference.Main/Controllers/UtilisateursController.cs
+++ VisioConference.Main/Controllers/UtilisateursController.cs
@@ -1,39 +1,161 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using VisioConference.DAO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using VisioConference.Data;
 using VisioConference.Models;
-using VisioDAO.DAO;
 
 namespace VisioConference.Main.Controllers
 {
     public class UtilisateursController : Controller
     {
-        private readonly IUtilisateurDAO _utilisateurDAO;
+        private readonly MyContext _context;
 
         public UtilisateursController(MyContext context)
         {
-            _utilisateurDAO = new UtilisateurDAO(context);
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Utilisateurs
+        public async Task<IActionResult> Index()
         {
-            return View();
+              return View(await _context.Utilisateur.ToListAsync());
         }
 
-        public IActionResult CreateUtilisateur()
+        // GET: Utilisateurs/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
-        }
-
-        public async Task<IActionResult> CreateUtilisateur([Bind("Id,Nom,Prenom,MotDePasse,DateDeNaissance,Email,SalonsCrees,UtilisateursSalons,Utilisateurs,Collegues")] Utilisateur utilisateur)
-        {
-            if(ModelState.IsValid)
+            if (id == null || _context.Utilisateur == null)
             {
-                await _utilisateurDAO.AddUtilisateur(utilisateur);
-                return RedirectToAction(nameof(Index));
+                return NotFound();
+            }
+
+            var utilisateur = await _context.Utilisateur
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (utilisateur == null)
+            {
+                return NotFound();
             }
 
             return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Utilisateurs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nom,Prenom,MotDePasse,DateDeNaissance,Email")] Utilisateur utilisateur)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(utilisateur);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            var utilisateur = await _context.Utilisateur.FindAsync(id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateurs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nom,Prenom,MotDePasse,DateDeNaissance,Email")] Utilisateur utilisateur)
+        {
+            if (id != utilisateur.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(utilisateur);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UtilisateurExists(utilisateur.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            var utilisateur = await _context.Utilisateur
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateurs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Utilisateur == null)
+            {
+                return Problem("Entity set 'MyContext.Utilisateur'  is null.");
+            }
+            var utilisateur = await _context.Utilisateur.FindAsync(id);
+            if (utilisateur != null)
+            {
+                _context.Utilisateur.Remove(utilisateur);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UtilisateurExists(int id)
+        {
+          return _context.Utilisateur.Any(e => e.Id == id);
         }
     }
 }
