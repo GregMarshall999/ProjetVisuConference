75a535b1141e309136f359c901f5f61c97807f68
Gregory Marshall <gregory.marshall999@gmail.com>
2022-12-23 15:59:40 +0100

    Commit

---
+++ VisioConference.Main/Data/SalonUtilisateurViewModel.cs
@@ -0,0 +1,10 @@
+﻿using VisioConference.Models;
+
+namespace VisioConference.Main.Data
+{
+    public class SalonUtilisateurViewModel
+    {
+        public Salon salon { get; set; }
+        public ICollection<Utilisateur> collegues { get; set; }
+    }
+}
